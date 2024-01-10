06f511824070e2127ee04052498b583a6c59383d
ktarrant <kmtarrant13@gmail.com>
2024-01-10 01:47:15 -0500

    set up models and admin

--- recipes/recipe/models.py
+++ recipes/recipe/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Good(models.Model):
+    name = models.CharField(max_length=100)
+    package = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    good = models.ForeignKey(Good, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    unit = models.CharField(max_length=32)
+    prep_method = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.amount} {self.unit} {self.good} {self.prep_method}"
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    instructions = models.TextField(max_length=1000)
+    servings = models.FloatField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return self.name
--- recipes/recipes/settings.py
+++ recipes/recipes/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'recipe.apps.RecipeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
