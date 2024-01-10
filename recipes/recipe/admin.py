e034bd6f195276638b88a0f0b631b50a30f8374f
ktarrant <kmtarrant13@gmail.com>
2024-01-10 02:44:58 -0500

    model rework

--- recipes/recipe/admin.py
+++ recipes/recipe/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Recipe, Good, Ingredient
+from .models import Recipe, Good, RecipeIngredient, GoodIngredient, Ingredient
 
 admin.site.register(Good)
+admin.site.register(RecipeIngredient)
+admin.site.register(GoodIngredient)
 admin.site.register(Ingredient)
 admin.site.register(Recipe)
--- recipes/recipe/models.py
+++ recipes/recipe/models.py
@@ -9,14 +9,49 @@ class Good(models.Model):
         return self.name
 
 
-class Ingredient(models.Model):
+class GoodIngredient(models.Model):
     good = models.ForeignKey(Good, on_delete=models.CASCADE)
     amount = models.FloatField()
     unit = models.CharField(max_length=32)
-    prep_method = models.CharField(max_length=100)
+    prep_method = models.CharField(max_length=100, blank=True, default='')
 
     def __str__(self):
         return f"{self.amount} {self.unit} {self.good} {self.prep_method}"
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey("Recipe", on_delete=models.CASCADE)
+    multiple = models.FloatField()
+
+    def __str__(self):
+        return f"{self.multiple} x {self.recipe.name}"
+
+
+class Ingredient(models.Model):
+    good_ingredient = models.ForeignKey(GoodIngredient,
+                                        on_delete=models.CASCADE,
+                                        blank=True, null=True)
+    recipe_ingredient = models.ForeignKey(RecipeIngredient,
+                                          on_delete=models.CASCADE,
+                                          blank=True, null=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="%(app_label)s_%(class)s_thing1_or_thing2",
+                check=(
+                        models.Q(good_ingredient__isnull=True, recipe_ingredient__isnull=False)
+                        | models.Q(good_ingredient__isnull=False, recipe_ingredient__isnull=True)
+                ),
+            )
+        ]
+
+    def __str__(self):
+        if self.good_ingredient is not None:
+            return str(self.good_ingredient)
+        if self.recipe_ingredient is not None:
+            return str(self.recipe_ingredient)
+        return ""
 
 
 class Recipe(models.Model):
