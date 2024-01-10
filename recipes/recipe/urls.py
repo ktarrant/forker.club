644c294ec2b11e4cb6b9698a15899833a6f5f788
ktarrant <kmtarrant13@gmail.com>
2024-01-10 18:36:42 -0500

    update views

--- recipes/recipe/urls.py
+++ recipes/recipe/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /recipe/
     path("", views.index, name="index"),
+    # ex: /recipe/5/
+    path("<int:recipe_id>/", views.detail, name="detail"),
 ]
--- recipes/recipe/views.py
+++ recipes/recipe/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from .models import Recipe
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_recipe_list = Recipe.objects.order_by('name')[:5]
+    context = {"latest_recipe_list": latest_recipe_list}
+    return render(request, "recipe/index.html", context)
+
+
+def detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, "recipe/detail.html", {"recipe": recipe})
