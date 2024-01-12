98e8121db9d1ca1e019e969b57acee32ab721a88
ktarrant <kmtarrant13@gmail.com>
2024-01-12 17:58:23 -0500

    model cleanup

--- recipes/recipe/models.py
+++ recipes/recipe/models.py
@@ -1,3 +1,4 @@
+from typing import List
 from django.db import models
 from django.utils.translation import gettext_lazy
 from mezzanine.pages.models import Page, RichText
@@ -11,18 +12,8 @@ class Good(models.Model):
         return self.name
 
 
-class Recipe(Page, RichText):
-    servings = models.FloatField()
-    good_ingredients = models.ManyToManyField(Good, through="GoodIngredient",)
-    recipe_ingredients = models.ManyToManyField("Recipe", through="RecipeIngredient")
-
-    class Meta:
-        verbose_name = gettext_lazy("Recipe")
-        verbose_name_plural = gettext_lazy("Recipes")
-
-
 class GoodIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
     good = models.ForeignKey(Good, on_delete=models.CASCADE)
     amount = models.FloatField()
     unit = models.CharField(max_length=32)
@@ -33,10 +24,36 @@ class GoodIngredient(models.Model):
 
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    recipe_ingredient = models.ForeignKey(Recipe, on_delete=models.CASCADE,
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    recipe_ingredient = models.ForeignKey('Recipe', on_delete=models.CASCADE,
                                           related_name='recipe_ingredient')
     amount = models.FloatField()
 
     def __str__(self):
-        return f"{self.amount} servings {self.recipe_ingredient.name}"
+        return f"{self.amount} servings {self.recipe_ingredient.title}"
+
+
+class Recipe(Page, RichText):
+    servings = models.FloatField()
+    good_ingredients = models.ManyToManyField(Good, through=GoodIngredient)
+    recipe_ingredients = models.ManyToManyField("Recipe", through=RecipeIngredient)
+
+    class Meta:
+        verbose_name = gettext_lazy("Recipe")
+        verbose_name_plural = gettext_lazy("Recipes")
+
+    def get_good_ingredients(self) -> List[GoodIngredient]:
+        return GoodIngredient.objects.select_related().filter(recipe=self.id)
+
+    def get_recipe_ingredients(self) -> List[GoodIngredient]:
+        return RecipeIngredient.objects.select_related().filter(recipe=self.id)
+
+    def get_combined_good_ingredients(self):
+        ingredients = list(self.get_good_ingredients())
+        for recipe_ingredient in self.get_recipe_ingredients():
+            add_ingredients = list(recipe_ingredient.recipe_ingredient.get_combined_good_ingredients())
+            multiplier = recipe_ingredient.amount / recipe_ingredient.recipe_ingredient.servings
+            for ingredient in add_ingredients:
+                ingredient.amount *= multiplier
+            ingredients += add_ingredients
+        return ingredients
--- recipes/recipe/page_processors.py
+++ recipes/recipe/page_processors.py
@@ -5,8 +5,8 @@ from .models import Recipe, GoodIngredient, RecipeIngredient
 
 @processor_for(Recipe)
 def recipe_render(request, page):
-    good_ingredient_list = GoodIngredient.objects.select_related().filter(recipe=page.recipe.id)
-    recipe_ingredient_list = RecipeIngredient.objects.select_related().filter(recipe=page.recipe.id)
+    good_ingredient_list = page.recipe.get_good_ingredients()
+    recipe_ingredient_list = page.recipe.get_recipe_ingredients()
     context = {"good_ingredient_list": good_ingredient_list,
                "recipe_ingredient_list": recipe_ingredient_list}
     return render(request, "pages/recipe.html", context=context)
